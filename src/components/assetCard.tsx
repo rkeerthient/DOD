b0d5b9bc083c895cd94d67d56056f7116f97c4e7
rkeerthi <sunnykeerthi@gmail.com>
2023-07-07 18:50:38 +0530

    fixed

--- src/components/assetCard.tsx
+++ src/components/assetCard.tsx
@@ -5,14 +5,23 @@ import * as React from "react";
 const AssetCard = (props: CardProps<any>) => {
   const { result } = props;
   const { name } = result;
-  const { c_image, landingPageUrl } = result.rawData;
+  const { c_image, landingPageUrl, c_assetType } = result.rawData;
+  console.log(JSON.stringify(c_assetType));
 
   return (
     <a href={landingPageUrl}>
       <div className="m-1 relative group">
         <Image image={c_image} className="!max-w-none " />
+        <div className="absolute  bg-transparent bottom-5 right-5">
+          {c_assetType[0].toLowerCase() === "video" && (
+            <img
+              src="https://www.dvidshub.net/images/video-icon.svg"
+              alt=""
+              className="h-10 w-10"
+            />
+          )}
+        </div>
         <div className="absolute top-0 left-0 w-full h-full bg-black opacity-0 group-hover:opacity-50"></div>
-
         <div className="absolute bottom-0 left-0 p-2 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 text-sm">
           {name}
         </div>
